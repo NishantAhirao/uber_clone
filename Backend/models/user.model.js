6dbd78c2b75dc7a4f44d356dccedf33030cbeee8
NishantAhirao <ahiraonishant@gmail.com>
2024-12-17 11:43:03 +0530

    user authentication done

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
 
 
 userSchema.method.generateAuthTokken = function(){
-    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET );
+    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET,{expiresIn:'24h'} );
     return token;
 }
 
